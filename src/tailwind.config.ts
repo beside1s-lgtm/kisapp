5866dfd7a77e38d142652d78d5ccac478f7f0ef5
초등교사강지욱 <beside1s@kshcm.net>
2026-01-08 07:06:43 +0000

    여전히 디자인이 적용 안 되고 있어. 문자들이 겹치고 난리법썩이구나. 어떤 파일을 손 봐야 하는거야?

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -18,6 +18,11 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        headline: ["var(--font-space-grotesk)", "sans-serif"],
+        body: ["var(--font-noto-sans-kr)", "sans-serif"],
+        code: ["var(--font-source-code-pro)", "monospace"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -74,7 +79,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("tailwindcss-debug-screens")],
 } satisfies Config
 
 export default config
